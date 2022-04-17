b451ef80fa2b7af9913755551315940f78faf87c
xiangxu <iasonliu@gmail.com>
2022-04-17 16:38:44 +0800

    print message

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@
 // disable runtime entry
 #![no_main]
 
+mod vga_buffer;
+
+// panic will run this
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
-
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
@@ -14,14 +14,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(
+        vga_buffer::WRITER.lock(),
+        ", some numbers: {} {}",
+        42,
+        1.337
+    )
+    .unwrap();
     loop {}
 }
